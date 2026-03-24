b7ffcaa2b462a1d856fa05f4c5048dfe97496fd9
sydneyc62 <sydneyc62@nycstudents.net>
2026-03-24 19:11:08 +0000

    Free me.

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let rnd = (l,u) => {
 let rnd2 = (l,u) => Math.floor(Math.random()*(u-l) + l);
 
 let scene;
+let progress = localStorage.getItem('points') || 0;
+
+
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); //CSS Selector
@@ -18,127 +21,40 @@ window.addEventListener("DOMContentLoaded",function() {
     let tree = new Tree(x, 0 , z);
     
   }
-})
+});
 
-// function rndAnomalyPage(){
-//   let links = new Array();
-//   links = [anomaly1.html , anomaly2.html , anomaly3.html , anomaly4.html , anomaly5.html];
-
-//   let rndIndex = Math.floor(Math.Random() * links.length);
-
-//   let rndUrl = links[rndIndex];
-
-  
-
-// }
-
-
-// window.addEventListener("DOMContentLoaded",function() {
-  
-  
-
-//     let progress = localStorage.getItem('points') || 0;
-//     let verify = document.getElementById("verify");
-//     let verifyNeg = document.getElementById("verifyNeg");
-//     let player = document.getElementById("player");
-//     let valueDisplay = document.getElementById("valueDisplay");
-//     let sky = document.getElementById("sky");
-
-//     // if(d1 < 1){
-//     //     progress ++;
-//     //     valueDisplay.setAttribute('value', `Distance: ${d1}`);
-//     // }
-
-
-      
-//     if(window.location.pathname === '/'){
-//         progress = 0;
-
-//     }
-
-//     valueDisplay.setAttribute('value', `points: ${progress}`);
-
-//     setInterval(()=>{
-//         let d1 = distance(player, verify);
-//         let d2 = distance(player, verifyNeg)
-        
-
-//         if (d1<2){
-//             progress++;
-//             valueDisplay.setAttribute('value', `points: ${progress}`);
-//             localStorage.setItem('points', progress);
-//             player.setAttribute('position', ' 0 1 0')
-//             window.location.href = rndUrl;
-
-//             if(window.location.pathname === '/game.html'){
-//                 localStorage.setItem('points', 0);
-//             }
-//         }
-
-//         if(d2<2){
-//             progress--;
-//             valueDisplay.setAttribute('value', `points: ${progress}`);
-//             localStorage.setItem('points', progress);
-//             player.setAttribute('position', ' 0 1 0')
-//             window.location.href = 'game.html';
-
-//             if(window.location.pathname === `/${rndUrl}` ){
-//                 localStorage.setItem('points', 0);
-//             }
-            
-//         }
-        
-//     }, 100);
-
-    
-
-// })
 
   window.addEventListener("DOMContentLoaded",function(){
+    progress = 0;
+
+
+  document.querySelectorAll('.artifact').forEach(artifact => {
 
 
 
-     document.querySelectorAll('.artifact').forEach(artifact => {
 
-        artifact.addEventListener('click', function() {
-          artifact.setAttribute("opacity",0);
+    artifact.addEventListener('click', () => {
+      artifact.setAttribute('visible', 'false');
+      progress++;
+      valueDisplay.setAttribute('value', `points: ${progress}`);
+      localStorage.setItem('points', progress);
+      });
     });
-
-
-
-      
-
+  }); 
 
 
 
 
+// function distance(obj1,obj2){
+//   let x1 = obj1.object3D.position.x;
+//   let y1 = obj1.object3D.position.y;
+//   let z1 = obj1.object3D.position.z;
+//   let x2 = obj2.object3D.position.x;
+//   let y2 = obj2.object3D.position.y;
+//   let z2 = obj2.object3D.position.z;
 
-
-
-
-
-
-
-
-
-
-  });
-
-
-
-
-
-
-function distance(obj1,obj2){
-  let x1 = obj1.object3D.position.x;
-  let y1 = obj1.object3D.position.y;
-  let z1 = obj1.object3D.position.z;
-  let x2 = obj2.object3D.position.x;
-  let y2 = obj2.object3D.position.y;
-  let z2 = obj2.object3D.position.z;
-
-  let d = Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2) + Math.pow(z1-z2,2));
-  return d;
-}
+//   let d = Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2) + Math.pow(z1-z2,2));
+//   return d;
+// }
 
 
